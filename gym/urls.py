52635a336c0f4a2330a646eefe0b2b7d746e56dd
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-10-28 12:53:33 +0530

    Create urls.py
    written function to render landing page(index.html) in the browser

---
+++ gym/urls.py
@@ -0,0 +1,6 @@
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, get_object_or_404
+
+def index(request):
+    return render(request, 'index.html')
