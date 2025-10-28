134259e6d15cb00053bb3187135de5fc3a38dfd3
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-10-28 17:09:34 +0530

    Update views.py
    added function for rendering about page

--- gym/views.py
+++ gym/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about-us.html')
