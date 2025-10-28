c052afc230d30b55cbbb20fc6e14a8f9f0488bfa
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-10-28 12:45:58 +0530

    Update urls.py
    Connected both app (gym,user ) to global urls.py

--- gymmgmt/urls.py
+++ gymmgmt/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('gym.urls')),
+    path('users/', include('users.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
