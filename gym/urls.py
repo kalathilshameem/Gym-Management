ecbfc5b4314566a8c15667392f91a2b7763aed10
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-10-28 17:08:46 +0530

    Update urls.py
    added about url path

--- gym/urls.py
+++ gym/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import index
+from .views import index,about
 
 urlpatterns = [
     path('', index, name='index'),
+    path('', about,name='about')
 ]
