f4392ea7c375902c25ef6aeed48d373249f2c881
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-10-28 13:01:30 +0530

    Add authentication imports and redirect functionality for index page

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
