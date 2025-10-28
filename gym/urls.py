1b737c6674706e002cdec82bdbf8b3caf69b727f
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-10-28 13:01:06 +0530

    Remove unused imports and index view

--- gym/urls.py
+++ gym/urls.py
@@ -1,6 +1,1 @@
 
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect, get_object_or_404
-
-def index(request):
-    return render(request, 'index.html')
