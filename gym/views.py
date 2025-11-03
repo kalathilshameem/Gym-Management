38bd538503c5f72d49d9f9b5661bc894d789d2c4
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-11-03 09:05:08 +0530

    Update views.py

--- gym/views.py
+++ gym/views.py
@@ -1,5 +1,14 @@
+import datetime
+from datetime import datetime, timedelta
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from .models import ContactForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html')
