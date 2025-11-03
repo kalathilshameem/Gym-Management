dc06e3382af917324a5334841c1adf7b67e1e0da
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-11-03 05:58:58 +0530

    created Models for Contact form

--- gym/models.py
+++ gym/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=255)
+    contact_no = models.CharField(max_length=15)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
