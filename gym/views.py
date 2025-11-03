bbdc4ae0c56ac2d9eaad15be44c905f8815fc935
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-11-03 08:46:51 +0530

    added backend logic for contact.html

--- gym/views.py
+++ gym/views.py
@@ -6,3 +6,26 @@ def index(request):
 
 def about(request):
     return render(request, 'about-us.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+        
+
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        contact_no = request.POST.get("contact_no")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        contact = ContactForm(name=name, contact_no=contact_no, email=email, message=message)
+        contact.save()
+
+        messages.success(request, "Your message has been sent successfully!")
+        return redirect("contact")
+
+    return render(request, "contact.html")
+
+def contact_list(request):
+    contacts = ContactForm.objects.all().order_by('-created_at')
+    return render(request, 'contact_list.html', {'contacts': contacts})
