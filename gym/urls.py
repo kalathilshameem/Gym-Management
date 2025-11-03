f28af67f9012d91770ef6587bad4e98309b28051
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-11-04 03:10:00 +0530

    Update urls.py

--- gym/urls.py
+++ gym/urls.py
@@ -4,7 +4,7 @@ from .views import contact, contact_view, index,about, services
 
 urlpatterns = [
     path('', index, name='index'),
-    path('', about,name='about'),
+    path('about', about,name='about'),
     path('contact/', contact, name='contact'),
     path('contact-view/', contact_view, name='contact'),
     path('contact-submissions/', views.contact_list, name='contact_list'),
