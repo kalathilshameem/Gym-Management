c0f2cc65e5c6110c2b0d288ad0476a1059688bda
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-11-18 14:36:12 +0530

    Add member-related paths to urls.py

--- gym/urls.py
+++ gym/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('contact-view/', contact_view, name='contact'),
     path('contact-submissions/', views.contact_list, name='contact_list'),
     path('services/', services, name='services'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('members/', views.members_list, name='members_list'),
+    path('members/add/', views.add_member, name='add_member'),
+    path('members/edit/<int:id>/', views.edit_member, name='edit_member'),
+    path('members/delete/<int:member_id>/', views.delete_member, name='delete_member')
 ]
