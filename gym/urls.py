97fed61c13bac4e242a47e6598987a51f0c2f3d7
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-11-18 14:55:05 +0530

    Add member registration path to URLs

--- gym/urls.py
+++ gym/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('contact-submissions/', views.contact_list, name='contact_list'),
     path('services/', services, name='services'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.member_registration, name='member_registration'),
     path('members/', views.members_list, name='members_list'),
     path('members/add/', views.add_member, name='add_member'),
     path('members/edit/<int:id>/', views.edit_member, name='edit_member'),
