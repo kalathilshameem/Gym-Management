10c6320b73f07535d64c2d659da31754e7c581ea
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-11-20 00:00:36 +0530

    Update models.py

--- gym/models.py
+++ gym/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 
 class ContactForm(models.Model):
     name = models.CharField(max_length=255)
