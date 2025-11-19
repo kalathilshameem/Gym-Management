bbbc632c035466ce0e76b650d71c82a69b3a2887
kalathilShameem <152679040+kalathilshameem@users.noreply.github.com>
2025-11-19 23:48:44 +0530

    Import MemberForm and Member model in views.py

--- gym/views.py
+++ gym/views.py
@@ -3,7 +3,8 @@ from datetime import datetime, timedelta
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from .models import ContactForm
+from gym.forms import MemberForm
+from .models import ContactForm, Member
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
